cb2d7e1504c32981dc47cb237e53e9cedf2dccfa
David Reband <david.reband@webanizer.de>
2024-01-26 12:55:24 +0100

    test: exchange of addresse

--- src/config.js
+++ src/config.js
@@ -22,28 +22,24 @@ export const dialMultiaddrItems =  [
     { id:"/ip4/78.46.210.80/udp/4004/webrtc-direct/certhash/uEiA1KCyqxF7qbtyJpumrlZEB2BmvvLddpAoowlmltQogug/p2p/12D3KooWA6NB8Vz5ro22X3ws3DQWndQKbdNgt6UNzgUSGjS4e96Z", text: "webrtc-direct tiflis"}
 ]
 
-const multiaddrs = [ '/ip4/127.0.0.1/udp/9090/webrtc-direct/certhash/uEiAIbksoQ56yn3UPDn0k_abCkGBHCf79iUemkVXRn_Vy2g/p2p/12D3KooWKABDpSmjtXQRm1FmkwbRDMzLHYi83btQnxHMAYRpYH8k']
-const multiaddrs2 = [
-  //  '/ip4/192.168.0.103/udp/9090/webrtc-direct/certhash/uEiAIbksoQ56yn3UPDn0k_abCkGBHCf79iUemkVXRn_Vy2g/p2p/12D3KooWKABDpSmjtXQRm1FmkwbRDMzLHYi83btQnxHMAYRpYH8k', //Schewtschenko
-    //'/ip4/10.41.92.3/udp/9090/webrtc-direct/certhash/uEiAIbksoQ56yn3UPDn0k_abCkGBHCf79iUemkVXRn_Vy2g/p2p/12D3KooWKABDpSmjtXQRm1FmkwbRDMzLHYi83btQnxHMAYRpYH8k' //SOK
-     '/ip4/159.69.119.82/udp/9090/webrtc-direct/certhash/uEiAIh0DoA5Qk2xTpc_j58KZMvww9CQzN6UNgsJ-DTuM6XQ/p2p/12D3KooWF5fGyE4VeXMhSGd9rCwckyxCVkA6xfoyFJG9DWJis62v' //istanbul
-    // '/ip4/127.0.0.1/udp/9095/quic-v1/p2p/12D3KooWPuveUzbMedUNP9C43LpNqpsrcHfTwAVrw8ghyksxW8xF' //go universal connectivity
-  //  '/ip4/164.92.229.178/udp/9090/webrtc-direct/certhash/uEiCyG5zCRpky38iwqX6RCuNvaAumkTM0dexnyVXjMf6QLA/p2p/12D3KooWDsKuyhLWZwXXwFph1Tgd2CWs4RJFktsVKdQL94uHLsZv'
-     // "/ip4/159.69.119.82/udp/4004/quic-v1/webtransport/certhash/uEiAP75UYHU9lxxeQ43_u3U7PrL3eeb0aOBfw2ty7CjuSUA/certhash/uEiBhciKTRyUiuDHnFkpOD_i3bMRCuGT8olXrdFvteNV-uA/p2p/12D3KooWAu6KS53pN69d6WG7QWttL14LnodUkBjZ1LG7F73k58LM",
-     // "/ip4/159.69.119.82/udp/4005/webrtc-direct/certhash/uEiD3LuzNOsNyskWmWI_wPr8-FlWBhbsEKsH9x6fcEGwT7w/p2p/12D3KooWAu6KS53pN69d6WG7QWttL14LnodUkBjZ1LG7F73k58LM",
-     // "/ip4/78.46.210.80/udp/4005/quic-v1/webtransport/certhash/uEiAbaFR9QTo2Y6xu3VEK_r3mciXziAFA0jBWvKSBb4QaMA/certhash/uEiAk1c3NNp0-aAlwktq7FdzQDcTAqcHjjWiR99MqCkrgTw/p2p/12D3KooWA6NB8Vz5ro22X3ws3DQWndQKbdNgt6UNzgUSGjS4e96Z",
-     // "/ip4/78.46.210.80/udp/4004/webrtc-direct/certhash/uEiA1KCyqxF7qbtyJpumrlZEB2BmvvLddpAoowlmltQogug/p2p/12D3KooWA6NB8Vz5ro22X3ws3DQWndQKbdNgt6UNzgUSGjS4e96Z"
-     //    '/dnsaddr/bootstrap.libp2p.io/p2p/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN',
-     //    '/dnsaddr/bootstrap.libp2p.io/p2p/QmQCU2EcMqAqQPR2i9bChDtGNJchTbq5TbXJJ16u19uLTa',
-     //    '/dnsaddr/bootstrap.libp2p.io/p2p/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb',
-     //    '/dnsaddr/bootstrap.libp2p.io/p2p/QmcZf59bWwK5XFi76CZX8cbJ4BhTzzA3gU1ZjYZcYW3dwt'
+//const multiaddrs = public_env.SEED_NODES;
+
+
+const multiaddrs = [
+	import.meta.env.MODE != 'development'
+		? import.meta.env.VITE_SEED_NODES_DEV
+		: import.meta.env.VITE_SEED_NODES
 ];
+
+console.log('____', import.meta.env);
+
+
 const pubSubPeerDiscoveryTopics = [
     `dcontact._peer-discovery._p2p._pubsub`, // It's recommended but not required to extend the global space
     // '_peer-discovery._p2p._pubsub' // Include if you want to participate in the global space
 ]
 
-const bootstrapConfig = {list: multiaddrs2};
+const bootstrapConfig = {list: multiaddrs};
 export const config = {
     addresses: {
         // swarm: [address],
--- tests/test.spec.js
+++ tests/test.spec.js
@@ -1,87 +1,123 @@
 import { test, expect } from '@playwright/test';
-import puppeteer from 'puppeteer';
-const { chromium } = require('playwright');
+import { chromium } from 'playwright';
 
-/*
-test('test chromium', async () => {
-  const browser = await chromium.launch({
-    headless: false
-  });
-  const context = await browser.newContext();
-  const page = await context.newPage();
-});
-*/
+const users = [
+	{
+		identity: 'Alice',
+		firstname: 'Alice',
+		lastname: 'Maier',
+		street: 'Schulgasse 5',
+		zipcode: '84444',
+		city: 'Berlin',
+		country: 'Germany'
+	},
+	{
+		identity: 'Bob',
+		firstname: 'Bob',
+		lastname: 'Dylan',
+		street: 'Schulgasse 55',
+		zipcode: '565544',
+		city: 'Berlin',
+		country: 'Germany'
+	},
+	{
+		identity: 'Alice',
+		firstname: 'Alice',
+		lastname: 'Fox',
+		street: 'Schulgasse 150',
+		zipcode: '1111111',
+		city: 'Rom',
+		country: 'Italy'
+	}
+];
 
-test('test', async () => {
-  const browser = await chromium.launch({
+async function OpenNewBrowser(user) {
+	const browser = await chromium.launch({
 		headless: false
 	});
+
 	const context = await browser.newContext();
 	const page = await context.newPage();
 
+	//await page.goto('http://localhost:5173/');
+	await page.goto('http://www.decontact.xyz/');
 
 
-
-	await page.goto('http://localhost:5173/');
-	await page.getByRole('tab', { name: 'Settings' }).click();
+	
+	https: await page.getByRole('tab', { name: 'Settings' }).click();
 	await page.getByLabel('My Identity').click();
-	await page.getByLabel('My Identity').fill('Alice');
+	await page.getByLabel('My Identity').fill(user.identity);
 	await page.getByRole('tab', { name: 'My Address' }).click();
 	await page.getByPlaceholder('Enter firstname...').click();
-	await page.getByPlaceholder('Enter firstname...').fill('Alice');
+	await page.getByPlaceholder('Enter firstname...').fill(user.firstname);
 	await page.getByPlaceholder('Enter lastname...').click();
-	await page.getByPlaceholder('Enter lastname...').fill('Maier');
+	await page.getByPlaceholder('Enter lastname...').fill(user.lastname);
 	await page.getByPlaceholder('Enter street...').click();
-	await page.getByPlaceholder('Enter street...').fill('Schulgasse 5');
+	await page.getByPlaceholder('Enter street...').fill(user.street);
 	await page.getByPlaceholder('Enter zipcode...').click();
-	await page.getByPlaceholder('Enter zipcode...').fill('84359');
+	await page.getByPlaceholder('Enter zipcode...').fill(user.zipcode);
 	await page.getByPlaceholder('Enter city...').click();
-	await page.getByPlaceholder('Enter city...').fill('Simbach am Inn');
+	await page.getByPlaceholder('Enter city...').fill(user.city);
 	await page.getByPlaceholder('Enter country...').click();
-	await page.getByPlaceholder('Enter country...').fill('German');
-	await page.getByRole('button', { name: 'Add' }).click({ timeout: 50000});
+	await page.getByPlaceholder('Enter country...').fill(user.country );
+	await page.getByRole('button', { name: 'Add' }).click({ timeout: 50000 });
+	return page;
+}
 
-  
+test.beforeAll(async () => {
+	
+});
 
-  const browser2 = await chromium.launch({
-    headless: false
-  });
-  const context2 = await browser2.newContext();
-  const page2 = await context2.newPage();
-  await page2.goto('http://localhost:5173/');
-  await page2.getByRole('tab', { name: 'Settings' }).click({ timeout: 50000});
-	await page2.getByLabel('My Identity').click();
-	await page2.getByLabel('My Identity').fill('Bob');
-	await page2.getByRole('tab', { name: 'My Address' }).click();
-	await page2.getByPlaceholder('Enter firstname...').click();
-	await page2.getByPlaceholder('Enter firstname...').fill('Bob');
-	await page2.getByPlaceholder('Enter lastname...').click();
-	await page2.getByPlaceholder('Enter lastname...').fill('Maier2');
-	await page2.getByPlaceholder('Enter street...').click();
-	await page2.getByPlaceholder('Enter street...').fill('Schulgasse 10');
-	await page2.getByPlaceholder('Enter zipcode...').click();
-	await page2.getByPlaceholder('Enter zipcode...').fill('84359');
-	await page2.getByPlaceholder('Enter city...').click();
-	await page2.getByPlaceholder('Enter city...').fill('Simbach am Inn');
-	await page2.getByPlaceholder('Enter country...').click();
-	await page2.getByPlaceholder('Enter country...').fill('German');
-	await page2.getByRole('button', { name: 'Add' }).click();
 
-  await page2.getByRole('textbox').click({ timeout: 50000 });
-	await page2.getByRole('textbox').fill('Alice');
-	await page2.getByRole('button', { name: 'Scan Contact' }).click(); 
-  
-  await page2.getByRole('textbox').click({ timeout: 50000 });
+test.describe('exchange of addresse', () => {
+	test('test', async () => {
+		test.setTimeout(60000);
 
-	await page.getByRole('button', { name: 'Continue' }).click();
-  //await page2.getByRole('textbox').click({ timeout: 50000 });
- 
-  await page2.getByRole('button', { name: 'Continue' }).click();
+		const page = await OpenNewBrowser(users[0]);
+		const page2 = await OpenNewBrowser(users[1]);
 
-  //await page.getByRole('textbox').click();
-  //await page.getByRole('textbox').fill('Bob');
-  //await page.getByRole('button', { name: 'Scan Contact' }).click(); 
+		await page2.getByRole('textbox').click({ timeout: 50000 });
+		await page2.getByRole('textbox').fill(users[0].identity);
+		await page2.getByRole('button', { name: 'Scan Contact' }).click();
+		await page.getByRole('button', { name: 'Continue' }).click();
+		await page2.getByRole('button', { name: 'Continue' }).click();
+		await page.getByRole('textbox').click({ timeout: 50000 });
+		await page.getByRole('textbox').fill(users[1].identity);
+		await page.getByRole('button', { name: 'Scan Contact' }).click();
+		await page2.getByRole('button', { name: 'Continue' }).click();
+		await page.getByRole('button', { name: 'Continue' }).click();
+		await page.getByRole('row', { name: users[0].identity }).locator('label').click();
 
-  //await page2.getByRole('tab', { name: 'Settings' }).click();
+		await page.getByPlaceholder('Enter lastname...').click();
+		await page.getByPlaceholder('Enter lastname...').fill(users[2].lastname);
 
+		await page.getByPlaceholder('Enter street...').click();
+		await page.getByPlaceholder('Enter street...').fill(users[2].street);
+
+		await page.getByPlaceholder('Enter zipcode...').click();
+		await page.getByPlaceholder('Enter zipcode...').fill(users[2].zipcode);
+
+		await page.getByPlaceholder('Enter city...').click();
+		await page.getByPlaceholder('Enter city...').fill(users[2].city);
+
+		await page.getByPlaceholder('Enter country...').fill(users[2].country);
+		await page.getByRole('button', { name: 'Update' }).click();
+
+		await page2.getByRole('button', { name: 'Continue' }).click();
+
+		//await page2.getByRole('textbox').click({ timeout: 50000 });
+
+		await page2.getByRole('row', { name: users[0].identity }).locator('label').click();
+
+		await page.getByRole('tab', { name: 'Settings' }).click({ timeout: 50000 });
+		await page.getByLabel('My Identity').click({ timeout: 50000 });
+
+		await page.close();
+		await page2.close();
+	});
+});
+
+test.afterAll(async () => {
+  //await page.close();
+	//await page2.close();
 });
