33719d0ae730229633fa4b71401a9e66ef4cf3a0
David Reband <david.reband@webanizer.de>
2024-02-12 12:19:03 +0100

    Improved test cases for checking devices synchronization

--- tests/MyDevicesAutoSync.spec.js
+++ tests/MyDevicesAutoSync.spec.js
@@ -13,7 +13,7 @@ const users = [
 		street: 'Schulgasse 5',
 		zipcode: '84444',
 		did: '',
-		seed:'test test test test test test test test test test test test',
+		seed: process.env.SEED_TEST,
 		city: 'Berlin',
 		country: 'Germany'
 	}
@@ -42,9 +42,8 @@ async function initializeNewPage(browser, user) {
 			await page.evaluate(() => window.localStorage.clear());
 			await page.evaluate(() => window.sessionStorage.clear());
 			await page.getByRole('button', { name: 'Continue' }).click();
-			await page.getByRole('button', { name: 'Generate New' }).click();
+			await page.getByRole('button', { name: 'Enter Existing Seed Phrase' }).click();
 			await page.getByRole('tab', { name: 'Settings' }).click();
-
 			await page.getByLabel('Seed Phrase', { exact: true }).click();	
   		await page.getByLabel('Seed Phrase').fill(user.seed);
 			await page.getByRole('tab', { name: 'My Address' }).click();
@@ -57,7 +56,7 @@ async function initializeNewPage(browser, user) {
 }
 
 
-test.describe('test - device synchronization', () => {
+test.describe('Devices auto synchronization', () => {
 	let page, page2;
 
 	test.beforeEach(async ({ browser }) => {
@@ -66,30 +65,17 @@ test.describe('test - device synchronization', () => {
 		//page2 = await initializeNewPage(browser, users[0]);
 	});
 	
-	test('synchronization', async () => {
+	test('checkDeviceSync', async () => {
 		test.setTimeout(60000);
 
 		const context = await browser.newContext();
 			page2 = await context.newPage();
 			const page_url = process.env.PAGE_URL;
 			await page2.goto(page_url);
-
 			await page2.getByRole('button', { name: 'Continue' }).click();
-			await page2.getByRole('button', { name: 'Generate New' }).click();
-			await page2.getByRole('tab', { name: 'Settings' }).click();
-
-			await page2.getByLabel('Seed Phrase', { exact: true }).click();	
-  		await page2.getByLabel('Seed Phrase').fill(users[0].seed);
-
-			await page2.getByRole('tab', { name: 'Contacts' }).click();
-
-
-
-
-		await page2.getByRole('textbox').click({ timeout: 50000 });
-		await page2.getByRole('textbox').fill(users[0].did);
-		await page2.getByRole('button', { name: 'Scan Contact' }).click();
-		
+			await page2.getByRole('button', { name: 'Enter Existing Seed Phrase' }).click();
+			await page2.getByRole('tab', { name: 'Contacts' }).click();  
+  		await page2.getByLabel('Contacts').locator('span').click();		
 	});
 
 	test.afterEach(async () => {
