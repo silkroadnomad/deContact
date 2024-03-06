cd7309b046002d9f09bfede49766eee740f651ee
Nico Krause <nico@le-space.de>
2024-03-06 15:58:35 +0800

    added todos to code

--- tests/BasicAddressExchange.spec.js
+++ tests/BasicAddressExchange.spec.js
@@ -1,87 +1,14 @@
 import { test, expect } from '@playwright/test';
 import { chromium } from 'playwright';
-
 const browser = await chromium.launch({
-		headless: true
+	headless: true
 });
-
-const users = [
-	{
-		identity: 'Alice',
-		firstname: 'Alice',
-		lastname: 'Maier',
-		street: 'Schulgasse 5',
-		zipcode: '84444',
-		did: '',
-		city: 'Berlin',
-		country: 'Germany'
-	},
-	{
-		identity: 'Bob',
-		firstname: 'Bob',
-		lastname: 'Dylan',
-		street: 'Schulgasse 55',
-		zipcode: '565544',
-		did: '',
-		city: 'Berlin',
-		country: 'Germany'
-	},
-	{
-		identity: 'Alice',
-		firstname: 'Alice',
-		lastname: 'Fox',
-		street: 'Schulgasse 150',
-		zipcode: '1111111',
-		did: '',
-		city: 'Rom',
-		country: 'Italy'
-	}
-];
-
-async function fillInput(page, placeholder, value) {
-    await page.click(`[placeholder="${placeholder}"]`);
-    await page.fill(`[placeholder="${placeholder}"]`, value);
-}
-
-async function fillForm(page, user) {
-    await fillInput(page, 'Enter firstname...', user.firstname);
-    await fillInput(page, 'Enter lastname...', user.lastname);
-		await fillInput(page, 'Enter street...', user.street);
-		await fillInput(page, 'Enter zipcode...', user.zipcode);
-		await fillInput(page, 'Enter city...', user.city);		
-		await fillInput(page, 'Enter country...', user.country);
-		await page.getByLabel('Category').click();
-  	await page.getByText('Private').click();
-		await page.locator('label').filter({ hasText: 'our own address' }).click();
-}
-
-async function initializeNewPage(browser, user) {
-	try {
-			const context = await browser.newContext();
-			const page = await context.newPage();
-			const page_url = process.env.PAGE_URL;
-			await page.goto(page_url);
-			await page.evaluate(() => window.localStorage.clear());
-			await page.evaluate(() => window.sessionStorage.clear());			
-			await page.getByRole('button', { name: 'Continue' }).click();
-			await page.getByRole('button', { name: 'Generate New' }).click();
-			await page.getByRole('tab', { name: 'Settings' }).click();
-			await page.getByLabel('DID', { exact: true }).click();			
-			user.did = await page.getByLabel('DID', { exact: true }).inputValue();
-			await page.getByRole('tab', { name: 'My Address' }).click();
-			await fillForm(page, user);
-			await page.getByRole('button', { name: 'Add' }).click({ timeout: 50000 });
-			return page;
-	} catch (error) {
-        console.error('Error opening new page:', error);
-  }
-}
-
-test.describe('Simple exchange of adress between Alice and Bob', () => {
+test.describe('simple address exchange between alice and bob', () => {
 	let page, page2;
 
 	test.beforeEach(async ({ browser }) => {
 		test.setTimeout(50000);
+
 		page = await initializeNewPage(browser, users[0]);
 		page2 = await initializeNewPage(browser, users[1]);
 	});
@@ -89,12 +16,13 @@ test.describe('Simple exchange of adress between Alice and Bob', () => {
 	test('Alice and Bob can exchange addresses', async () => {
 		test.setTimeout(150000);
 
-
 		await page2.getByRole('tab', { name: 'Contacts' }).click({ timeout: 250000 });
 		await page2.getByRole('textbox', { role: 'scanContact' }).click();
 		await page2.getByRole('textbox', { role: 'scanContact' }).fill(users[0].did);
 		await page2.getByRole('button', { name: 'Scan Contact' }).click({ timeout: 150000 });
-		
+
+		//TODO Test if second browser is open
+		//TODO Test if second browser and first browser are connected (at least 2 peers connected)
 		//await page2.getByRole('button', { name: 'Scan Contact' }).click({ timeout: 150000 });
 
 		await page.getByRole('button', { name: 'Exchange Contact Data' }).click({ timeout: 50000 });
@@ -144,3 +72,75 @@ test.describe('Simple exchange of adress between Alice and Bob', () => {
 		]);
 	});
 });
+
+const users = [
+	{
+		identity: 'Alice',
+		firstname: 'Alice',
+		lastname: 'Maier',
+		street: 'Schulgasse 5',
+		zipcode: '84444',
+		did: '',
+		city: 'Berlin',
+		country: 'Germany'
+	},
+	{
+		identity: 'Bob',
+		firstname: 'Bob',
+		lastname: 'Dylan',
+		street: 'Schulgasse 55',
+		zipcode: '565544',
+		did: '',
+		city: 'Berlin',
+		country: 'Germany'
+	},
+	{
+		identity: 'Alice',
+		firstname: 'Alice',
+		lastname: 'Fox',
+		street: 'Schulgasse 150',
+		zipcode: '1111111',
+		did: '',
+		city: 'Rom',
+		country: 'Italy'
+	}
+];
+
+async function fillInput(page, placeholder, value) {
+	await page.click(`[placeholder="${placeholder}"]`);
+	await page.fill(`[placeholder="${placeholder}"]`, value);
+}
+
+async function fillForm(page, user) {
+	await fillInput(page, 'Enter firstname...', user.firstname);
+	await fillInput(page, 'Enter lastname...', user.lastname);
+	await fillInput(page, 'Enter street...', user.street);
+	await fillInput(page, 'Enter zipcode...', user.zipcode);
+	await fillInput(page, 'Enter city...', user.city);
+	await fillInput(page, 'Enter country...', user.country);
+	await page.getByLabel('Category').click();
+	await page.getByText('Private').click();
+	await page.locator('label').filter({ hasText: 'our own address' }).click();
+}
+
+async function initializeNewPage(browser, user) {
+	try {
+		const context = await browser.newContext();
+		const page = await context.newPage();
+		const page_url = process.env.PAGE_URL;
+		await page.goto(page_url);
+		await page.evaluate(() => window.localStorage.clear());
+		await page.evaluate(() => window.sessionStorage.clear());
+		await page.getByRole('button', { name: 'Continue' }).click();
+		await page.getByRole('button', { name: 'Generate New' }).click();
+		await page.getByRole('tab', { name: 'Settings' }).click();
+		await page.getByLabel('DID', { exact: true }).click();
+		user.did = await page.getByLabel('DID', { exact: true }).inputValue();
+		await page.getByRole('tab', { name: 'My Address' }).click();
+		await fillForm(page, user);
+		await page.getByRole('button', { name: 'Add' }).click({ timeout: 50000 });
+		return page;
+	} catch (error) {
+		console.error('Error opening new page:', error);
+	}
+}
